7a0eef1b8b307e83d2a5ad30d4d12bbce5b26018
sabrinaabeloe <76934251+sabrinaabeloe@users.noreply.github.com>
2021-06-14 14:52:07 -0700

    js add

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(".usda").hover("mouseenter", function(){
+$(this).css("background-color", "white").inFunction("background-color","#c8b2e4e8");  
+});
+$(".gsoc").hover("mouseenter", function(){
+ $(this).css("background-color", "white").inFunction("background-color","#c8b2e4e8");
+});
