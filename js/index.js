95aa9eb66fb8c33db15c84b2f619ce025f54aceb
sabrinaabeloe <76934251+sabrinaabeloe@users.noreply.github.com>
2021-06-19 11:41:04 -0700

    v2 hotjar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
-$(".usda").hover("mouseenter", function(){
-$(this).css("background-color", "white").inFunction("background-color","#c8b2e4e8");  
-});
-$(".gsoc").hover("mouseenter", function(){
- $(this).css("background-color", "white").inFunction("background-color","#c8b2e4e8");
-});
+$('.usda').hover(
+    function(){
+        // CHANGES THE BACKGROUND COLOR
+        $(this).css('background-color', '#c8b2e4e8')
+        // A POINTER CURSOR LETS THE USER KNOW THAT THIS IS A CLICKABLE ELEMENT 
+        $(this).css('cursor', 'pointer')
+    }, function() {
+        // RETURNS COLOR BACK TO NORMAL AFTER MOUSE IS NO LONGER HOVERING 
+        $(this).css('background-color', '')
+    });
+    $('.gsoc').hover(
+        function(){
+            // CHANGES THE BACKGROUND COLOR
+            $(this).css('background-color', '#c8b2e4e8')
+            // A POINTER CURSOR LETS THE USER KNOW THAT THIS IS A CLICKABLE ELEMENT 
+            $(this).css('cursor', 'pointer')
+        }, function() {
+            // RETURNS COLOR BACK TO NORMAL AFTER MOUSE IS NO LONGER HOVERING 
+            $(this).css('background-color', '')
+        });
+     // end document.ready function - all jquery code goes above this line
