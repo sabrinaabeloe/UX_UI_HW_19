570fd50727994ea9fe0b90430d300d700521a667
sabrinaabeloe <76934251+sabrinaabeloe@users.noreply.github.com>
2021-06-19 23:14:37 -0700

    v2

--- js/index.js
+++ js/index.js
@@ -18,4 +18,45 @@ $('.usda').hover(
             // RETURNS COLOR BACK TO NORMAL AFTER MOUSE IS NO LONGER HOVERING 
             $(this).css('background-color', '')
         });
+$('#work').hover(
+    function(){
+        $(this).css('font-weight','bold')
+        $(this).css('cursor','pointer')
+    }, function() {
+        $(this).css('font-weight','')
+    });
+    $('#contact').hover(
+        function(){
+            $(this).css('font-weight','bold')
+            $(this).css('cursor','pointer')
+        }, function() {
+            $(this).css('font-weight','')
+        });
+        $('#skills').hover(
+            function(){
+                $(this).css('font-weight','bold')
+                $(this).css('cursor','pointer')
+            }, function() {
+                $(this).css('font-weight','')
+            });
+            $('#about').hover(
+                function(){
+                    $(this).css('font-weight','bold')
+                    $(this).css('cursor','pointer')
+                }, function() {
+                    $(this).css('font-weight','')
+                });
+                const sr = ScrollReveal({
+                    origin: 'top',
+                    distance: '80px',
+                    duration: 2000,
+                    reset: true});
+
+                    sr.reveal('.logo',{}); 
+sr.reveal('.nav',{delay: 200}); 
+sr.reveal('.heroImg',{delay: 400}); 
+sr.reveal('.mySkills',{ interval: 200});
+sr.reveal('.workGrid',{delay:200}); 
+sr.reveal('.allProjects',{delay:200});
+sr.reveal('.footer',{delay:200});
      // end document.ready function - all jquery code goes above this line
